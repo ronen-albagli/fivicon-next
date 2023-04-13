1a0806665e8f912e711ed57a828727db94699e97
ronen.albagli@gmail.com <ronen.albagli@gmail.com>
2023-04-14 01:03:08 +0300

    feat: add ga and og tags

--- src/utils/ga.ts
+++ src/utils/ga.ts
@@ -1,6 +1,6 @@
 import ReactGA from 'react-ga';
 
-export const initGA = (trackingID) => {
+export const initGA = (trackingID: string) => {
   ReactGA.initialize(trackingID, { gaOptions: { siteSpeedSampleRate: 100 } });
 };
 
@@ -8,7 +8,7 @@ export const logPageView = () => {
   ReactGA.pageview(window.location.pathname);
 };
 
-export const logEvent = ({ category, action, label, value }) => {
+export const logEvent = ({ category, action, label, value }: any) => {
   ReactGA.event({
     category,
     action,
