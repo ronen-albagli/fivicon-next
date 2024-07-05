69549a392278dbf736cb41934629ed8119385b2e
Ronen Albagli <ronen.albagli@gmail.com>
2024-07-05 14:42:19 +0300

    feat: add redirect to new site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(props: any) {
   };
 
   useEffect(() => {
-    window.location.href = 'https://fivicon.com/lp';
+    fetchFiviStats();
   }, []);
 
   useEffect(() => {
