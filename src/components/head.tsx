b7655045abf879e35ead27b0a636d7ae728456e1
Ronen Albagli <ronen.albagli@gmail.com>
2024-09-18 17:01:45 +0300

    fix: header

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -40,6 +40,13 @@ export const AppHead = () => {
       <link
         href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;700&family=Rubik:wght@600&display=swap"
         rel="stylesheet"></link>
+
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800;8..144,900;8..144,1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+      />
+      <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro&family=Space+Grotesk:wght@700&display=swap"
+        rel="stylesheet"></link>
     </Head>
   );
 };
